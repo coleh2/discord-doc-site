9acd0a6e5fc5a0dd39bba518aef576771c85e2e3
coleh <coleh@coleh.net>
2020-06-22 20:43:13 -0400

    Recognize comments when parsing Highcharts expressions

--- build-scripts/parse-js-expression.js
+++ build-scripts/parse-js-expression.js
@@ -1,60 +1,62 @@
-if(typeof module === 'object') module.exports = parseExpression;
+if (typeof module === 'object') module.exports = parseExpression;
 
 function parseExpression(str) {
     str = str.trim();
-    if(!str) return null;
+    if (!str) return null;
+
+    str = stripComments(str);
 
     let result
     //Object
-    if(str.startsWith("{") && str.endsWith("}")) {
-        let objBody = str.substring(1,str.length - 1);
+    if (str.startsWith("{") && str.endsWith("}")) {
+        let objBody = str.substring(1, str.length - 1);
         let keyVals = groupAwareSplit(objBody, ",");
 
         result = {};
-        for(var i = 0; i < keyVals.length; i++) {
+        for (var i = 0; i < keyVals.length; i++) {
             let keyVal = keyVals[i];
 
-            let lhsRhsSeperatorIndex = findUngroupedChar(keyVal, ":");
+            let lhsRhsSeperatorIndex = findUngroupedSubstring(keyVal, ":");
 
-            let lhs = keyVal.substring(0,lhsRhsSeperatorIndex).trim();
-            let rhs = keyVal.substring(lhsRhsSeperatorIndex+1).trim();
+            let lhs = keyVal.substring(0, lhsRhsSeperatorIndex).trim();
+            let rhs = keyVal.substring(lhsRhsSeperatorIndex + 1).trim();
 
-            if(lhs) result[unQuote(lhs)] = parseExpression(rhs.trim());
+            if (lhs) result[unQuote(lhs)] = parseExpression(rhs.trim());
         }
     }
     //Array
     else if (str.startsWith("[") && str.endsWith("]")) {
-        let arrBody = str.substring(1,str.length - 1);
+        let arrBody = str.substring(1, str.length - 1);
         let vals = groupAwareSplit(arrBody, ",");
 
         result = [];
-        for(var i = 0; i < vals.length; i++) {
+        for (var i = 0; i < vals.length; i++) {
             result.push(parseExpression(vals[i]));
         }
     }
-    //True/False
-    else if(str == "true" || str == "false") {
+    //Boolean
+    else if (str == "true" || str == "false") {
         result = (str == "true");
     }
     //Function
-    else if(str.startsWith("function")) {
+    else if (str.startsWith("function")) {
         let functionExpression = str.substring("function".length).trim();
 
         let functionName = /^(\w+)/.exec(functionExpression);
-        if(functionName) {
+        if (functionName) {
             functionName = functionName[1];
-            
-            functionExpression = functionExpression.substring(functionName.length).trim();   
+
+            functionExpression = functionExpression.substring(functionName.length).trim();
         }
 
         let arguments = /\(([^)]*)\)/.exec(functionExpression)[1];
 
         let argList = arguments.split(",");
 
-        for(var i = 0; i < argList.length; i++) argList[i] = argList[i].trim();
+        for (var i = 0; i < argList.length; i++) argList[i] = argList[i].trim();
 
         let body = functionExpression.substring(arguments.length + 2).trim();
-        body = body.substring(1,body.length-1);
+        body = body.substring(1, body.length - 1);
 
         result = {
             __type: "function",
@@ -65,10 +67,10 @@ function parseExpression(str) {
 
         let safety = safetyCheckFunction(result);
 
-        if(!safety.safe) throw "Safety check on function failed. " + safety.reason;
+        if (!safety.safe) throw "Safety check on function failed. " + safety.reason;
     }
     //Number
-    else if(!isNaN(str)) {
+    else if (!isNaN(str)) {
         result = parseFloat(str);
     }
     //String
@@ -79,8 +81,61 @@ function parseExpression(str) {
     return result;
 }
 
+function stripComments(str) {
+
+    let commentStart, commentEnd;
+
+    let inSingleQuote = false, inDoubleQuote = false, inMultilineComment = false;
+
+    for (var i = 0; i < str.length; i++) {
+        let inQuote;
+        if(!inMultilineComment) {
+            if (str[i] == "\"" && str[i - 1] != "\\" && !inSingleQuote) inDoubleQuote = !inDoubleQuote;
+            if (str[i] == "'" && str[i - 1] != "\\" && !inDoubleQuote) inSingleQuote = !inSingleQuote;
+            inQuote = inSingleQuote || inDoubleQuote;
+        }
+        
+
+        if (!inQuote && !inMultilineComment) {
+            //inline comment
+            if (str[i] == "/" && str[i + 1] == "/") {
+                commentEnd = indexOfAfter(str,"\n",i);
+
+                //if it's on the last line, commentEnd should be the end of the string
+                commentEnd = commentEnd != -1 ? commentEnd : str.length;
+
+                //with the length change, i is now at the end
+                str = indexReplace(str, i, commentEnd, "\n");
+            }
+
+            //multiline comment
+            if(str[i] == "/" && str[i + 1] == "*") {
+                commentStart = i;
+                inMultilineComment = true;
+            }
+        }
+
+        //only the specific character sequence can end multilines, and only when one is already on
+        if(str[i] == "*" && str[i + 1] == "/" && inMultilineComment) {
+            commentEnd = i+1;
+            str = indexReplace(str, commentStart, commentEnd, "");
+            inMultilineComment = false;
+        }
+    }
+    return str;
+}
+
+
+function indexOfAfter(str, substr, index) {
+    return str.substring(index+1).indexOf(substr) + index + 1;
+}
+
+function indexReplace(str, replaceStart, replaceEnd, replaceText) {
+    return str.substring(0, replaceStart) + replaceText + str.substring(replaceEnd + 1);
+}
+
 //TODO: write a proper safety checker (variable usage list, safe variables, etc.)
-function safetyCheckFunction(func) {
+function safetyCheckFunction(funcObj) {
     return {
         safe: false,
         reason: "At the moment, functions are not allowed due to vulnerabilities when running them."
@@ -88,12 +143,12 @@ function safetyCheckFunction(func) {
 }
 
 function unQuote(str) {
-    if(str.startsWith("\"") && str.endsWith("\"")) {
-        return str.substring(1,str.length-1);
+    if (str.startsWith("\"") && str.endsWith("\"")) {
+        return str.substring(1, str.length - 1);
     }
-    
-    if(str.startsWith("'") && str.endsWith("'")) {
-        return str.substring(1,str.length-1);
+
+    if (str.startsWith("'") && str.endsWith("'")) {
+        return str.substring(1, str.length - 1);
     }
 
     return str;
@@ -104,16 +159,16 @@ function findUngroupedCharRegex(str, regex) {
     let inSingleQuote = false;
     let inDoubleQuote = false;
 
-    for(var i = 0; i < str.length; i++) {
-        if(str[i] == "\"" && str[i-1] != "\\" && !inSingleQuote) inDoubleQuote = !inDoubleQuote;
-        if(str[i] == "'" && str[i-1] != "\\" && !inDoubleQuote) inSingleQuote = !inSingleQuote;
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] == "\"" && str[i - 1] != "\\" && !inSingleQuote) inDoubleQuote = !inDoubleQuote;
+        if (str[i] == "'" && str[i - 1] != "\\" && !inDoubleQuote) inSingleQuote = !inSingleQuote;
 
         let inQuote = inSingleQuote || inDoubleQuote;
 
-        if((str[i] == "{" || str[i] == "[") && !inQuote) inGroup++;
+        if ((str[i] == "{" || str[i] == "[") && !inQuote) inGroup++;
         else if ((str[i] == "}" || str[i] == "]") && !inQuote) inGroup--;
 
-        if(str[i].match(regex) && inGroup == 0 && inQuote == false) return i;
+        if (str[i].match(regex) && inGroup == 0 && inQuote == false) return i;
     }
     return -1;
 }
@@ -124,15 +179,15 @@ function groupAwareSplit(str, split) {
     split = split || " ";
 
     let prevSplitIndex = 0,
-        nextSplitIndex = findUngroupedChar(str, split);
-        
+        nextSplitIndex = findUngroupedSubstring(str, split);
 
-    while(nextSplitIndex != -1 && words.length < 1000) {
+
+    while (nextSplitIndex != -1 && words.length < 1000) {
         words.push(str.substring(prevSplitIndex, nextSplitIndex));
 
-        prevSplitIndex = nextSplitIndex+1;
-        nextSplitIndex = findUngroupedChar(str.substring(prevSplitIndex), split);
-        if(nextSplitIndex > -1) nextSplitIndex += prevSplitIndex;
+        prevSplitIndex = nextSplitIndex + 1;
+        nextSplitIndex = findUngroupedSubstring(str.substring(prevSplitIndex), split);
+        if (nextSplitIndex > -1) nextSplitIndex += prevSplitIndex;
     }
 
     words.push(str.substring(prevSplitIndex));
@@ -140,21 +195,52 @@ function groupAwareSplit(str, split) {
     return words;
 }
 
-function findUngroupedChar(str, char) {
+/**
+ * A collection of options governing how to find groups.
+ * @typedef {Object} GroupFindingOptions
+ * @property {boolean} doQuotes - Treat quotes as groups
+ * @property {boolean} doGroups - Use usual grouping symbols ([], {}, ())
+ */
+
+/**
+ * 
+ * @param {String} str The string to search in
+ * @param {String} substr The string to search for
+ * @param {Object} [options] - Options that govern how groups are defined
+ * @param {boolean} options.doQuotes - Treat quotes as groups
+ * @param {boolean} options.doGroups - Also use usual grouping symbols ([], {}, ())
+ * @returns First index of the substring inside the string, or -1 if none were found
+ */
+function findUngroupedSubstring(str, substr, options) {
     let inGroup = 0;
     let inSingleQuote = false;
     let inDoubleQuote = false;
 
-    for(var i = 0; i < str.length; i++) {
-        if(str[i] == "\"" && str[i-1] != "\\" && !inSingleQuote) inDoubleQuote = !inDoubleQuote;
-        if(str[i] == "'" && str[i-1] != "\\" && !inDoubleQuote) inSingleQuote = !inSingleQuote;
+    //fill in defaults
+    if (!options) options = {
+        doQuotes: true,
+        doGroups: true
+    }
+
+    for (var i = 0; i < str.length; i++) {
+
+        if (options.doQuotes) {
+            if (str[i] == "\"" && str[i - 1] != "\\" && !inSingleQuote) inDoubleQuote = !inDoubleQuote;
+            if (str[i] == "'" && str[i - 1] != "\\" && !inDoubleQuote) inSingleQuote = !inSingleQuote;
+        }
 
         let inQuote = inSingleQuote || inDoubleQuote;
 
-        if((str[i] == "{" || str[i] == "[") && !inQuote) inGroup++;
-        else if ((str[i] == "}" || str[i] == "]") && !inQuote) inGroup--;
+        if (options.doGroups) {
+            if ((str[i] == "{" || str[i] == "[") && !inQuote) inGroup++;
+            else if ((str[i] == "}" || str[i] == "]") && !inQuote) inGroup--;
+        }
 
-        if(str[i] == char && inGroup == 0 && inQuote == false) return i;
+        if (str.substring(i - substr.length + 1, i + 1) == substr &&
+            inGroup == 0 &&
+            inQuote == false) {
+            return i - substr.length;
+        }
     }
     return -1;
 }
@@ -164,16 +250,16 @@ function hasUngroupedChar(str, char) {
     let inSingleQuote = false;
     let inDoubleQuote = false;
 
-    for(var i = 0; i < str.length; i++) {
-        if(str[i] == "\"" && str[i-1] != "\\" && !inSingleQuote) inDoubleQuote = !inDoubleQuote;
-        if(str[i] == "'" && str[i-1] != "\\" && !inDoubleQuote) inSingleQuote = !inSingleQuote;
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] == "\"" && str[i - 1] != "\\" && !inSingleQuote) inDoubleQuote = !inDoubleQuote;
+        if (str[i] == "'" && str[i - 1] != "\\" && !inDoubleQuote) inSingleQuote = !inSingleQuote;
 
         let inQuote = inSingleQuote || inDoubleQuote;
 
-        if((str[i] == "{" || str[i] == "[") && !inQuote) inGroup++;
+        if ((str[i] == "{" || str[i] == "[") && !inQuote) inGroup++;
         else if ((str[i] == "}" || str[i] == "]") && !inQuote) inGroup--;
 
-        if(str[i] == char && inGroup == 0 && !inQuote) return true;
+        if (str[i] == char && inGroup == 0 && !inQuote) return true;
     }
     return false;
 }
