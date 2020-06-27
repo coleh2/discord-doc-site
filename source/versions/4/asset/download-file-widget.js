10584769d4dda800de316f147fcbe5aff4065aaf
coleh <coleh@coleh.net>
2020-06-27 12:36:14 -0400

    minor SEO and accessibility changes

--- source/versions/4/asset/download-file-widget.js
+++ source/versions/4/asset/download-file-widget.js
@@ -8,6 +8,8 @@ window.addEventListener("load", function() {
         
         let downloaderIcon = document.createElement("img");
         downloaderIcon.src = "data:image/svg+xml,%3Csvg width='24' height='24' xmlns='http://www.w3.org/2000/svg' fill-rule='evenodd' clip-rule='evenodd'%3E%3Cpath d='M22 24h-20v-24h14l6 6v18zm-7-23h-12v22h18v-16h-6v-6zm1 5h4.586l-4.586-4.586v4.586z'/%3E%3C/svg%3E";
+        downloaderIcon.alt = "Generic file icon";
+        
         downloader.appendChild(downloaderIcon);
 
         let textToDownload = downloader.querySelector("code").innerText;
