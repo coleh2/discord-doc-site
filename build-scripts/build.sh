4c2c6b3e657d0671a36ffa9c99f64c01f232237e
coleh2 <coleh@coleh.net>
2020-09-06 19:34:25 -0400

    Unify build scripts

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,4 +1,7 @@
-export NHS_RULES_DIR="."
+#if on github, cd to directory
+export NHS_RULES_DIR=$GITHUB_WORKSPACE
+
+cd "$NHS_RULES_DIR"
 
 #clean build directory of old artifacts
 rm -rf './build'
@@ -6,5 +9,6 @@ rm -rf './build'
 # copy source files to build directory-- this is so that assets maintain their relative positions
 cp -r './source/' './build'
 
+# run markdown building and script minifying
 node build-scripts/build-markdown.js
 node build-scripts/minify-scripts.js
